68f432920101035f59fddbac5d1b189de30a840f
Eddy Diaz Garcia <eddymanueldiazgarcia@hotmail.com>
2026-07-31 23:25:53 -0400

    feat: agregar clases y objetos.

---
+++ ClasesYObjetos/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+var sale = new Sale(100, DateTime.Now);
+Console.WriteLine(sale.GetInfo());
+
+
+class Sale
+{
+    public int total;
+    public DateTime date;
+
+    public Sale(int total, DateTime date)
+    {
+        this.total = total;
+        this.date = date;
+    }
+
+    public string GetInfo()
+    {
+        return $"Total: {total}, Date: {date.ToLongDateString()}";
+    }
+
+    
+
+
+}
